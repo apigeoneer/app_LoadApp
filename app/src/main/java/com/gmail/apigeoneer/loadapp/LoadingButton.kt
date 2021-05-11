280fd18a021a4e355c67ec66c0a650dba756b92e
Chitranshi Srivastava <apigeoneer@gmail.com>
2021-05-11 20:33:26 +0530

    Added Custom View class LoadingButton.kt. Overridden the methods onDraw() & onMeasure() in it.

--- app/src/main/java/com/gmail/apigeoneer/loadapp/LoadingButton.kt
+++ app/src/main/java/com/gmail/apigeoneer/loadapp/LoadingButton.kt
@@ -1,11 +1,9 @@
 package com.gmail.apigeoneer.loadapp
 
-import android.animation.ValueAnimator
 import android.content.Context
+import android.graphics.Canvas
 import android.util.AttributeSet
 import android.view.View
-import java.util.jar.Attributes
-import kotlin.properties.Delegates
 
 class LoadingButton @JvmOverloads constructor(
     context: Context,
@@ -16,12 +14,20 @@ class LoadingButton @JvmOverloads constructor(
     private var widthSize = 0
     private var heightSize = 0
 
-    private val valueAnimator = ValueAnimator()
-
-    private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
-
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
     }
 
-
-
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val minw: Int = paddingLeft + paddingRight + suggestedMinimumWidth
+        val w: Int = resolveSizeAndState(minw, widthMeasureSpec, 1)
+        val h: Int = resolveSizeAndState(
+            MeasureSpec.getSize(w),
+            heightMeasureSpec,
+            0
+        )
+        widthSize = w
+        heightSize = h
+        setMeasuredDimension(w, h)
+    }
 }
