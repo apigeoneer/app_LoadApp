1470f3882bf12bd3df15d3889f5edade35008ac0
Chitranshi Srivastava <apigeoneer@gmail.com>
2021-05-11 23:02:26 +0530

    Set an onClickListener to the download button. Called a download fun i/s this listener. Created this download fun.

--- app/src/main/java/com/gmail/apigeoneer/loadapp/MainActivity.kt
+++ app/src/main/java/com/gmail/apigeoneer/loadapp/MainActivity.kt
@@ -5,20 +5,54 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
+import androidx.databinding.DataBindingUtil
+import com.gmail.apigeoneer.loadapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val URL = "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
+    }
+
+    // data binding
+    private lateinit var binding: ActivityMainBinding
+
+    private var downloadID: Long = 0
+
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
+
+        binding.downloadBtn.setOnClickListener {
+            download()
+        }
     }
 
     private val receiver = object: BroadcastReceiver() {
         override fun onReceive(p0: Context?, p1: Intent?) {
             val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
         }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun download() {
+        val request =
+            DownloadManager.Request(Uri.parse(URL))
+                .setTitle(getString(R.string.app_name))
+                .setDescription(getString(R.string.app_description))
+                .setRequiresCharging(false)
+                .setAllowedOverMetered(true)
+                .setAllowedOverRoaming(true)
+
+        val downloadManager = getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+        downloadID = downloadManager.enqueue(request)        // enqueue puts the download request in the queue
     }
 }
