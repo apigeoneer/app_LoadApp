3ec1eb25cd950ddaeb9b630ccbf280671224adcd
Chitranshi Srivastava <apigeoneer@gmail.com>
2021-05-11 20:35:31 +0530

    Add a var receiver which stores a BroadcastReceiver obj. Override the method onReceive() inside it.

--- app/src/main/java/com/gmail/apigeoneer/loadapp/MainActivity.kt
+++ app/src/main/java/com/gmail/apigeoneer/loadapp/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.gmail.apigeoneer.loadapp
 
+import android.app.DownloadManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -9,4 +13,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
+    private val receiver = object: BroadcastReceiver() {
+        override fun onReceive(p0: Context?, p1: Intent?) {
+            val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+        }
+    }
 }
