3883a154e7d8f92647a6cc3fbc2be130b223d239
Chitranshi Srivastava <apigeoneer@gmail.com>
2021-05-11 20:38:17 +0530

    register the Download receiver just created inside onCreate()

--- app/src/main/java/com/gmail/apigeoneer/loadapp/MainActivity.kt
+++ app/src/main/java/com/gmail/apigeoneer/loadapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.DownloadManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -11,6 +12,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
     }
 
     private val receiver = object: BroadcastReceiver() {
