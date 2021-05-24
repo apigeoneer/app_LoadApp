58a85078ea592c06be3fd474a1ab15d566cb6480
Chitranshi Srivastava <apigeoneer@gmail.com>
2021-05-25 00:34:00 +0530

    Removed the encapsulating class from NotificationUtils

--- app/src/main/java/com/gmail/apigeoneer/loadapp/MainActivity.kt
+++ app/src/main/java/com/gmail/apigeoneer/loadapp/MainActivity.kt
@@ -28,22 +28,23 @@ class MainActivity : AppCompatActivity() {
 
     // data binding
     private lateinit var binding: ActivityMainBinding
+
     private lateinit var notificationManager: NotificationManager
     private var urlSelected = "https://github.com/bumptech/glide.git"       // default: Glide
     private var repoSelected = "Glide repo"
-    private var downloadID: Long = 0
-    private val loadingButton = LoadingButton(this)
+
+    private val downloadUtils = DownloadUtils(this, binding.downloadCv, notificationManager)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         // register the download receiver
-        registerReceiver(receiver,
+        registerReceiver(downloadUtils.receiver,
                 IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
 
         binding.downloadCv.setOnClickListener {
-            download(urlSelected, repoSelected)
+            downloadUtils.download(urlSelected, repoSelected)
         }
 
         notificationManager = ContextCompat.getSystemService(
@@ -74,40 +75,6 @@ class MainActivity : AppCompatActivity() {
                     }
             }
         }
-    }
-
-    private val receiver = object: BroadcastReceiver() {
-        override fun onReceive(context: Context?, intent: Intent?) {
-            val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
-
-            if (id == downloadID) {
-                Toast.makeText(context, "File downloaded", Toast.LENGTH_SHORT).show()
-                Log.d(TAG, "Downloaded")
-                // reset the download button
-                loadingButton.setButtonState(ButtonState.Completed)
-            }
-
-            notificationManager.sendNotification(
-                    "Your download was successful!",
-                    context!!
-            )
-        }
-    }
-
-    private fun download(selectedURL: String, selectedRepo: String) {
-        // set the download button to the loading state
-        loadingButton.setButtonState(ButtonState.Loading)
-
-        val request =
-                DownloadManager.Request(Uri.parse(selectedURL))
-                        .setTitle(selectedRepo)
-                        .setDescription("Downloading $selectedRepo from internet")
-                        .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE)
-                        .setAllowedOverMetered(true)
-                        .setAllowedOverRoaming(true)
-
-        val downloadManager = getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
-        downloadID = downloadManager.enqueue(request)        // enqueue puts the download request in the queue
     }
 
     private fun createChannel(channelId: String, channelName: String) {
