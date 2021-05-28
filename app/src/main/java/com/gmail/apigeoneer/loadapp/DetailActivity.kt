09935b0fc46b67efc1104ba46cf1d0130b1fa87a
Chitranshi Srivastava <apigeoneer@gmail.com>
2021-05-28 22:41:09 +0530

    Navigated back to the MainActivity from the DetailActivity

--- app/src/main/java/com/gmail/apigeoneer/loadapp/DetailActivity.kt
+++ app/src/main/java/com/gmail/apigeoneer/loadapp/DetailActivity.kt
@@ -1,11 +1,23 @@
 package com.gmail.apigeoneer.loadapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class DetailActivity : AppCompatActivity() {
+
+    private var fileName = ""
+    private var status = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+
+        findViewById<Button>(R.id.ok_btn).setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
